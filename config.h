24722495ba2e9accef0ad07c966b7c599b8157fd
tetr-a <tetr-a@tetr.com>
2024-09-09 06:42:03 +0700

    fixing some update intervals

--- config.h
+++ config.h
@@ -18,8 +18,9 @@ static const Block blocks[] = {
 	/*{"",	"sb-forecast",	18000,	5},*/
 	/*{"",	"sb-mailbox",	180,	12},*/
 	{"",	"sb-battery",	5,	3},
-	{"",	"sb-volume",	1,	16},
-	{"",	"sb-internet",	5,	4},
+	{"",	"sb-internet",	0,	4},
+	{"",	"sb-volume",	0,	16},
+	{"",	"sb-music",	1,	10},
 	{"",	"sb-clock",	60,	1},
 	/* {"",	"sb-iplocate", 0,	27}, */
 	/*{"",	"sb-help-icon",	0,	15},*/
@@ -27,8 +28,3 @@ static const Block blocks[] = {
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char *delim = " | ";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
